d5755be6736d59712f775834029450a5e2a67774
weiz-98 <qweqwe928928@gmail.com>
2024-05-19 01:52:24 +0800

    feat: add configuration settings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// Define a new command-line flag with the name 'addr', a default value of ":4000"
+	// and some short help text explaining what the flag controls. The value of the
+	// flag will be stored in the addr variable at runtime.
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	// Importantly, we use the flag.Parse() function to parse the command-line flag.
+	// This reads in the command-line flag value and assigns it to the addr
+	// variable. You need to call this *before* you use the addr variable
+	// otherwise it will always contain the default value of ":4000". If any errors are
+	// encountered during parsing the application will be terminated.
+	flag.Parse()
 	// Use the http.NewServeMux() function to initialize a new servemux, then
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
@@ -26,13 +37,16 @@ func main() {
 
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	log.Println("Starting server on :4000")
+	log.Printf("Starting server on %s", *addr)
 	// Use the http.ListenAndServe() function to start a new web server. We pass in
 	// two parameters: the TCP network address to listen on (in this case ":4000")
-	// and the servermux we just created. If http.ListenAndServe() returns an error
-	// we use the log.Fatal() function to log the error message and exit. Note
-	// that any error returned by http.ListenAndServe() is always non-nil.
-	err := http.ListenAndServe(":4000", mux)
+	// and the servermux we just created.
+
+	// The value returned from the flag.String() function is a pointer to the flag
+	// value, not the value itself. So we need to dereference the pointer (i.e.
+	// prefix it with the * symbol) before using it. Note that we're using the
+	// log.Printf() function to interpolate the address with the log message.
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
 
