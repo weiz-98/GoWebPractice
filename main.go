eefa1aba8eb621aeeda98eaf580d4d03a544d541
weiz-98 <qweqwe928928@gmail.com>
2024-05-18 18:53:25 +0800

    fix: add customizing headers (Uncommon practice)

--- main.go
+++ main.go
@@ -23,7 +23,17 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 // Add a snippetCreate handler function.
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Create a new snippet..."))
+	// Use r.Method to check whether the request is using POST or not.
+	if r.Method != "POST" {
+		// If it's not, use the w.WriteHeader() method to send a 405 status // code and the w.Write() method to write a "Method Not Allowed"
+		// response body. We then return from the function so that the
+		// subsequent code is not executed.
+		w.WriteHeader(406)
+		w.Write([]byte("This Method Not Allowed")) // 和 WriteHeader 的訊息是獨立的
+		return
+	}
+	w.Write([]byte("This Method Is Allowed"))
+
 }
 func main() {
 	// Use the http.NewServeMux() function to initialize a new servemux, then
