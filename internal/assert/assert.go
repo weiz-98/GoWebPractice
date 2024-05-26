7288e9a3cf3a3089be483c574c7674fc487d9311
weiz-98 <qweqwe928928@gmail.com>
2024-05-27 01:02:29 +0800

    feat: add TestShowSnippet

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -1,6 +1,7 @@
 package assert
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -9,5 +10,12 @@ func Equal[T comparable](t *testing.T, actual, expected T) {
 	// Helper 讓 Go 測試運行器將在輸出中報告呼叫 Equal() 函數的程式碼的檔案名稱和行號
 	if actual != expected {
 		t.Errorf("got: %v; want: %v", actual, expected)
+	}
+}
+
+func StringContains(t *testing.T, actual, expectedSubstring string) {
+	t.Helper()
+	if !strings.Contains(actual, expectedSubstring) {
+		t.Errorf("got: %q; expected to contain: %q", actual, expectedSubstring)
 	}
 }
